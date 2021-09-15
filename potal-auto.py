a1dd5e732c3a5eca93eed2fb16283ec624a8bd68
あきみど <65846606+AKI-MIDO@users.noreply.github.com>
2021-09-16 01:38:21 +0900

    update py
    隠すべきところを隠した。

--- potal-auto.py
+++ potal-auto.py
@@ -6,26 +6,26 @@ import time
 # ポータルサイトへの自動ログイン -
 #----------------------------
 
-driver_path = "/Users/endouakira/Desktop/chromedriver2"
+driver_path = "パスの位置"
 driver = webdriver.Chrome(driver_path)
 
 #ポータルサイトのログイン画面にアクセス
-driver.get("https://t-portal.main.teikyo-u.ac.jp/start/auth/login?error=L00002")
+driver.get("サイトのURL")
 
 
 #ログインID（学籍番号）の入力
-id = driver.find_element_by_id("loginId")
-id.send_keys("19E122009")
+id = driver.find_element_by_id("----")
+id.send_keys("ログイID")
 
 #パスワードの入力
-password = driver.find_element_by_id("passwordId")
-password.send_keys("1Endoakira.")
+password = driver.find_element_by_id("------")
+password.send_keys("パスワード")
 
 #ログインidとパスワードを確実に入力させる
 time.sleep(0.5)
 
 #上記でIDとパスワードを入力後にログインのボタンを押すイベントを発生
-login_button = driver.find_element_by_id('loginPost')
+login_button = driver.find_element_by_id('-----')
 login_button.click()
 
 
@@ -34,12 +34,12 @@ login_button.click()
 #------------------------------------
 
 #簡易ポータルから通常ポータルへ移動
-potal_move = driver.find_element_by_xpath('//a[@href="/start/auth/after/notice"]')
+potal_move = driver.find_element_by_xpath('---------')
 potal_move.click()
 
 
 #インフォメーションの項目に移動
-imformation = driver.find_element_by_xpath('//*[@id="standard"]/dl[6]/dt/div/figure/a')
+imformation = driver.find_element_by_xpath('/------------')
 imformation.click()
 
 #ここで一旦時間をおかないとxpathが取れない。
@@ -47,7 +47,7 @@ time.sleep(5.0)
 
 cur_url=driver.current_url
 #print(cur_url)
-detail_info = driver.find_element_by_css_selector('#searchTbody > tr:nth-child(1) > td:nth-child(5)')
+detail_info = driver.find_element_by_css_selector('------------')
 txt=detail_info.text
 print(detail_info.text)
 
@@ -56,7 +56,7 @@ print(detail_info.text)
 #-----------------
 
 notify_url = 'https://notify-api.line.me/api/notify'
-notify_token = 'R8xBcHMw6iuTdvpkZH189GjVFrTgeeIrQ6cnI3DREZw'
+notify_token = '----------------'
 
 def main():
     #line notifyの設定
@@ -64,7 +64,7 @@ def main():
     headers = {"Authorization" : "Bearer "+ notify_token}
 
     #現在保存されている情報を確認
-    f1=open("/Users/endouakira/Desktop/大学の研究発表/test.txt","r",encoding="UTF-8")
+    f1=open("----------------","r",encoding="UTF-8")
     last_text=f1.read()
     f1.close()
     
@@ -82,7 +82,7 @@ def main():
         requests.post(notify_url, headers = headers, params = payload)
         
         #保存する情報を更新
-        f1=open("/Users/endouakira/Desktop/大学の研究発表/test.txt","w",encoding="UTF-8")
+        f1=open("-----------------","w",encoding="UTF-8")
         f1.write(txt)
         f1.close()
         driver.quit()
